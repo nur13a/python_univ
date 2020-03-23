67d4154c0b63fc366b1ce0c09ce586bb4b788abb
Nuriza <nuriza.samidinovna@gmail.com>
2020-03-24 01:12:06 +0600

    lab1,2,3,4

---
+++ laboratornaya rabota№1/laboratornaya_rabota_1.py
@@ -0,0 +1,81 @@
+#n=int(input("Введите первое число: "))
+#m=int(input("Введите второе число: "))
+#print("Сумма: ")
+#print(n+m)
+#print("Разность: ")
+#print(n-m)
+#print("Деление: ")
+#print(n/m)
+#print("Умножение: ")
+#print(n*m)
+#print("Остаток от деления: ")
+#print(n%m)
+
+#Task 2
+#floor=int(input("Введите номер квартиры: "))
+#if floor<=4:
+ #   print("Квартира на 1ом этаже")
+#elif floor<=8: 
+ #   print("Квартира на 2ом этаже")
+#elif floor<=12:
+ #   print("Квартира на 3ом этаже")
+#elif floor<=16:
+ #   print("Квартира на 4ом этаже")
+#elif floor<=20:
+ #   print("Квартира на 5ом этаже")
+#elif floor<=24:
+ #   print("Квартира на 6ом этаже")
+#elif floor<=28:
+ #   print("Квартира на 7ом этаже")
+#elif floor<=32:
+ #   print("Квартира на 8ом этаже")
+#elif floor<=36:
+ #   print("Квартира на 9ом этаже")
+
+ #Task 5
+#m=int(input("Введите первое число: "))
+#n=int(input("Введите второе число: "))
+#if m%n==0:
+ #   print(m/n)
+#else:
+ #   print("m на n нацело не делится")
+
+ #Task 8
+#import random
+#money = 3
+#print("выберите орел или решка. если вводете 0, то выбираете орла, 1-решку, любое другое число – конец игры")
+#while money>0:
+#    choose=int(input("введите ваш выбор: "))
+#    hh=random.randint(0,1)
+#    if choose==1 or choose==0:
+#        print(hh)
+#        if choose==1 and hh==1:
+#            money+=1
+#            print(money)
+#        elif choose==1 and hh==0:
+#            money-=1
+#            print(money)
+#        elif choose==0 and hh==1:
+#            money-=1
+#            print(money)
+#        elif choose==0 and hh==0: 
+#            money+=1 
+#            print(money)
+#    else: 
+#        print("error, you must choose 0 or 1")
+
+ #Task 7
+#for i in range(11):
+#    print(i,"* 5 =",i*5)
+
+#Task 6
+#number =int(input("Enter number: ")) 
+#num=0
+#for i in range(number):
+#    num=i*i;
+#    if num<number:
+#        print(num)
+
+
+
+ #Task9
